cb1c20ba9456f6116244ec07a4165e1fb77e22c5
guopeng li <guopeng.li@technicolor.com>
2021-01-31 15:56:51 +0100

    Using spinkit package for loading screen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const api_key = '7b29eacf5429c8e7058298fa20e7d6fd';
 
@@ -43,6 +44,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
